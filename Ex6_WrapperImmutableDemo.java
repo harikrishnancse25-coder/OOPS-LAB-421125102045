26414d69f46bbf3b49af81834349e0bf09b15fb3
harikrishnancse25-coder <harikrishnan.cse25@ifet.ac.in>
2026-09-24 23:19:01 +0530

    Add files via upload

---
+++ Ex6_WrapperImmutableDemo.java
@@ -0,0 +1,27 @@
+public class Ex6_WrapperImmutableDemo {
+    public static void main(String args[]) {
+        // Integer Wrapper
+        Integer i = Integer.valueOf(100);
+        System.out.println("Before Modification");
+        System.out.println("Integer Value : " + i);
+        System.out.println("Hash Code     : " + System.identityHashCode(i));
+
+        // Modification
+        i = i + 50;
+        System.out.println("\nAfter Modification");
+        System.out.println("Integer Value : " + i);
+        System.out.println("Hash Code     : " + System.identityHashCode(i));
+
+        // Double Wrapper
+        Double d = Double.valueOf(25.5);
+        System.out.println("\nBefore Modification");
+        System.out.println("Double Value  : " + d);
+        System.out.println("Hash Code     : " + System.identityHashCode(d));
+
+        // Modification
+        d = d * 2;
+        System.out.println("\nAfter Modification");
+        System.out.println("Double Value  : " + d);
+        System.out.println("Hash Code     : " + System.identityHashCode(d));
+    }
+}
